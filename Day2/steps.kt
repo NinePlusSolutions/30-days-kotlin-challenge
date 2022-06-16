bf3c82b2cf90e4a3eadde98ee184c35545b9500f
Bao Cong <ngobaocong.it@gmail.com>
2022-06-16 13:32:30 +0700

    update(for_repeat): can type a number from input

--- Day2/steps.kt
+++ Day2/steps.kt
@@ -1,6 +1,7 @@
 fun main() {
-    val n = 5
-    for (index in 1..n) {
+    println("Enter number: ")
+    var inputNumber = Integer.valueOf(readLine())
+    for (index in 1..inputNumber) {
         // repeat same the multiply a string
         println("#".repeat(index))
     }
