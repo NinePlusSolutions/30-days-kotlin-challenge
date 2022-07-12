7417d49e3f827bc0a6d0fdd5b9f2830ccd166b5d
Bao Cong <ngobaocong.it@gmail.com>
2022-07-12 09:16:11 +0700

    build(Service): complete built Service

---
+++ Service/app/src/main/java/com/example/service/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.service
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.service.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnStart.setOnClickListener {
+                startService(Intent(this@MainActivity,MusicService::class.java))
+            }
+
+            btnStop.setOnClickListener {
+                stopService(Intent(this@MainActivity,MusicService::class.java))
+            }
+        }
+
+    }
+}
+
+
